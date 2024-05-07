b87d5c926118adb2322682cefbdf01506ef1d656
laurapadoan40 <laurapadoan40@gmail.com>
2024-05-07 11:03:32 -0300

    exercício4.py

---
+++ exercicio_4.py
@@ -0,0 +1,6 @@
+# Entrada de Dados
+nota1 = float(input("insira a primeira nota: "))
+nota2 = float(input("insira a segunda nota: "))
+nota3 = float(input("insira a terceira nota: "))
+média = (nota1+nota2+nota3/3)
+print(f"a média de suas notas foram de: {média}")
