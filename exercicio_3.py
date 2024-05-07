9d75616e4e38476def5ddc1aac4eaf54ff9c553e
laurapadoan40 <laurapadoan40@gmail.com>
2024-05-07 10:45:32 -0300

    exercício3.py

---
+++ exercicio_3.py
@@ -0,0 +1,5 @@
+# Entrada de Dados
+base = int(input("insira a base do triângulo: "))
+altura = int(input("insira a altura do triângulo: "))
+área = (base*altura/2)
+print(f"a área do triângulo é: {área}")
