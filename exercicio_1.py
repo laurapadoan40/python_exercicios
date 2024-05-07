62fd36f86599e440810efe40c99c5b28365e5a9a
laurapadoan40 <laurapadoan40@gmail.com>
2024-05-07 09:37:03 -0300

    exercicio1.py

---
+++ exercicio_1.py
@@ -0,0 +1,6 @@
+#Entrada de dados
+numero1 = int(input("Digite um número inteiro: "))
+numero2 = int(input("Digite um outro numero intero: "))
+soma = (numero1 + numero2)
+produto = (numero1 +numero2)
+print(f"A soma dos números citados à cima tem valor de:{soma}"f" O produto dos números:{produto}")
