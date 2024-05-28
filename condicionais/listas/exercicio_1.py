5dee35b974bdb002c1298c3ae006e8198aadc2f5
laurapadoan40 <laurapadoan40@gmail.com>
2024-05-28 10:51:37 -0300

    lista_vazia.py

--- condicionais/listas/exercicio_1.py
+++ condicionais/listas/exercicio_1.py
@@ -3,4 +3,11 @@ produtos = {
     2:['Teclado Wireless', 49.26, 1],
     3:['Mouse Wireless', 19.90. 1],
     4:['Cartucho Colorido', 54.00, 2]
-    }
+}
+total = 0 
+
+print(50 '-')
+print('CARRINHO DE COMPRAS')
+print(50 * '-')
+
+for cod, prod 
