02bd1947f24416b05829716da6b7c001d333b427
laurapadoan40 <laurapadoan40@gmail.com>
2024-05-28 10:31:29 -0300

    alinhadas_listas.py

---
+++ condicionais/listas/listas_alinhadas.py
@@ -0,0 +1,13 @@
+listas_compras = []
+
+num_produtos = int(input("Quantos produtos vc deseja adicionar?": ))
+
+for i in range (num_produtos):
+    produto = input("Nome do produto: ")
+    quantidade = int(input("Quantidade: "))
+    listas_compras.append([produto,quantidade])
+
+    print("Lista de cmpras: ")
+    for produto, quantidade: in listas_compras
+         print ;(f"Produto: {produto} - Qtd: {quantidade}")
+    
