7d31a093e90c55b08055ba01349731b2dbd2f124
laurapadoan40 <laurapadoan40@gmail.com>
2024-06-04 14:22:31 -0300

    exercício1.py

--- condicionais/funções/exercício1.py
+++ condicionais/funções/exercício1.py
@@ -2,6 +2,12 @@ def calcularMedia(nota1, nota2, nota3, nota4):
     media = (nota1, nota2, nota3, nota4)/4
     return media
 
+def resultado(media):
+    if (media >= 5):
+        return "Aprovado"
+    else:
+        return "Reprovado"
+    
 print("Calculadora de Média")
 n1 = float(input("Digite a 1º nota: "))
 n2 = float(input("Digite a 2º nota: "))
